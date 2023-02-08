7a232dfeb8a9303d76a89036bb78662f2aac8f49
Ash <acst52@gmail.com>
2023-02-08 13:31:36 -0500

    Imported Classes & added basic Employee questions

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-// runs the application, generates index.html into the dist directory
 // let's start with the require statements to import modules:
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
-const generateHTML = require('./src/generateHTML')
+
+// now let's import our classes from the classes.js file in lib:
+const Manager = require('./lib/classes');
+const Engineer = require('./lib/classes');
+const Intern = require('./lib/classes');
+
+let employee = [];
+
+// now let's create a fcn to initialize the application. Note: here, the 
+  // createEmployee fcn returns a promise that, when resolved, creates 'employee' obj 
+  // which contains all user's answers collected through the inquirer.prompt calls:
+const createEmployee = async () => {
+  // Let's first ascertain their job title:
+    const roleAnswers = await inquirer.prompt([
+      {
+        type: 'list',
+        name: 'role',
+        message: 'What is your role?',
+        choices: ['Manager', 'Engineer', 'Intern']
+      }
+    ]);
+
+  // let's get all the common questions in one place:
+    const commonAnswers = await inquirer.prompt([
+      { name: 'name', message: 'What is your name? ' },
+      { name: 'id', message: 'What is your employee ID? ' },
+      { name: 'email', message: 'What is your employee email? ' }, // **Validate user input
+    ]);
