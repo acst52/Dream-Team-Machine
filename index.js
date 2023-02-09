aad2fdc5facd62b20944a41fc9fa685c65f9a990
Ash <acst52@gmail.com>
2023-02-09 01:41:18 -0500

    Changed direction; making sep fcns for ea role

--- index.js
+++ index.js
@@ -9,23 +9,51 @@ const Intern = require('./lib/classes');
 
 let employee = [];
 
-// now let's create a fcn to initialize the application. Note: here, the 
-  // createEmployee fcn returns a promise that, when resolved, creates 'employee' obj 
-  // which contains all user's answers collected through the inquirer.prompt calls:
-const createEmployee = async () => {
-  // Let's first ascertain their job title:
-    const roleAnswers = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'role',
-        message: 'What is your role?',
-        choices: ['Manager', 'Engineer', 'Intern']
-      }
-    ]);
+// now lets create fcns for each Employee type that contain the common + role-specific Qs
+function createManager() {
+      return new Promise((resolve) => {
+            inquirer.prompt([
+            { name: 'name', message: 'What is your name? ' },
+            { name: 'id', message: 'What is your employee ID? ' },
+            { name: 'email', message: 'What is your employee email? ' },
+      ]).then(commonAnswers => {
+            inquirer.prompt([
+            { name: 'officeNumber', message: 'office number: ' },
+            ]).then(managerAnswers => {
+            resolve(new Manager(commonAnswers.name, commonAnswers.id, commonAnswers.email, managerAnswers.officeNumber));
+      })
+   })
+});
+}
+        
+function createEngineer() {
+      return new Promise((resolve) => {
+            inquirer.prompt([
+            { name: 'name', message: 'What is your name? ' },
+            { name: 'id', message: 'What is your employee ID? ' },
+            { name: 'email', message: 'What is your employee email? ' },
+      ]).then(commonAnswers => {
+            inquirer.prompt([
+              { name: 'github', message: 'GitHub username: ' },
+            ]).then(engineerAnswers => {
+            resolve(new Engineer(commonAnswers.name, commonAnswers.id, commonAnswers.email, engineerAnswers.github));
+      })
+   })
+});
+}
 
-  // let's get all the common questions in one place:
-    const commonAnswers = await inquirer.prompt([
-      { name: 'name', message: 'What is your name? ' },
-      { name: 'id', message: 'What is your employee ID? ' },
-      { name: 'email', message: 'What is your employee email? ' }, // **Validate user input
-    ]);
+function createIntern() {
+      return new Promise((resolve) => {
+            inquirer.prompt([
+            { name: 'name', message: 'What is your name? ' },
+            { name: 'id', message: 'What is your employee ID? ' },
+            { name: 'email', message: 'What is your employee email? ' },
+      ]).then(commonAnswers => {
+            inquirer.prompt([
+                  { name: 'school', message: 'school attended: ' },
+            ]).then(internAnswers => {
+            resolve(new Intern(commonAnswers.name, commonAnswers.id, commonAnswers.email, internAnswers.school));
+      })
+   })
+});
+}
