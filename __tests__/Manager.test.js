b2ade5cdab95fac8280f29c9e0845482f95d0c3e
Ash <acst52@gmail.com>
2023-04-19 13:03:26 -0400

    update Manager import - destructured

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,4 @@
-// Jest tests
-
-const Manager = require('./lib/classes');
+const { Manager } = require('../lib/classes');
 
 describe('Manager', () => {
     describe('getOfficeNumber', () => {
@@ -17,34 +15,3 @@ describe('Manager', () => {
       });
     });
   });
-
-
-// OR:
-
-// describe('Manager', () => {
-//     let manager;
-  
-//     beforeEach(() => {
-//       manager = new Manager('Ashley', 1234, 'acst52@test.com', 101);
-//     });
-  
-//     it('should return the name of the manager', () => {
-//       expect(manager.getName()).toBe('Ashley');
-//     });
-  
-//     it('should return the id of the manager', () => {
-//       expect(manager.getId()).toBe(1234);
-//     });
-  
-//     it('should return the email of the manager', () => {
-//       expect(manager.getEmail()).toBe('acst52@test.com');
-//     });
-  
-//     it('should return the office number of the manager', () => {
-//       expect(manager.getOfficeNumber()).toBe(101);
-//     });
-  
-//     it('should return the role of the manager', () => {
-//       expect(manager.getRole()).toBe('Manager');
-//     });
-// });
