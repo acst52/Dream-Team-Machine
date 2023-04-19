79c6cf21b160494c017f5e4cebe3cdeab7c820c5
Ash <acst52@gmail.com>
2023-04-19 12:35:09 -0400

    add engineer jest tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,19 @@
 // Jest tests
+
+const Engineer = require('./lib/classes');
+
+describe('Engineer', () => {
+    describe('getGithub', () => {
+      it('should return the GitHub username of the engineer', () => {
+        const engineer = new Engineer('Ashley', 1234, 'acst52@test.com', 'acst52');
+        expect(engineer.getGithub()).toEqual('acst52');
+      });
+    });
+  
+    describe('getRole', () => {
+      it('should return the role of the engineer', () => {
+        const engineer = new Engineer('Ashley', 1234, 'acst52@test.com', 'acst52');
+        expect(engineer.getRole()).toEqual('Engineer');
+      });
+    });
+  });
