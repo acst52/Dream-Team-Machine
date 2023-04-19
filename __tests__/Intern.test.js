a4329e3475a6eba0538a7b2e7d227e39388ed305
Ash <acst52@gmail.com>
2023-04-19 12:35:29 -0400

    add intern jest tests

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,1 +1,19 @@
 // Jest tests
+
+const Intern = require('./lib/classes');
+
+describe('Intern', () => {
+    describe('getSchool', () => {
+      it('should return the school of the intern', () => {
+        const intern = new Intern('Ashley', 1234, 'acst52@test.com', 'Carleton U');
+        expect(intern.getSchool()).toEqual('Carleton U');
+      });
+    });
+  
+    describe('getRole', () => {
+      it('should return the role of the intern', () => {
+        const intern = new Intern('Ashley', 1234, 'acst52@test.com', 'Carleton U');
+        expect(intern.getRole()).toEqual('Intern');
+      });
+    });
+  });
